2631cfaca19e92cb8abe238a2b3f03cc4d26ba6b
omnlgy <ikballukmanulhakim5@gmail.com>
2026-06-19 19:40:50 +0700

    basic net htpp

---
+++ cmd/api/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	fmt.Println("Server is running on port 8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		fmt.Println(err)
+	}
+}
